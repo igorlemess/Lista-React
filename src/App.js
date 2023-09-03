4db8e477bce1d86714f9b5df48d441bf2535c9ff
igorlemess <igorramosirl11@gmail.com>
2023-09-02 21:13:51 -0300

    Forms criados

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
 
+import {useState, useEffect} from 'react'
+import {BsTrash, BsBookmarkCheck, BsBookmarckCheckFill} from 'react-icons/bs';
+
+const API = "http://localhost:5000";
+
 function App() {
+  const [title,setTitle] = useState("")
+  const [qtde,setQtde] = useState("")
+  const [itens,setitens] = useState([])
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const item = {
+      id: Math.random(),
+      title,
+      qtde,
+      done: false,
+    };
+
+    console.log(item)
+
+    setTitle("");
+    setQtde("");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="lista-header">
+        <h1>Lista</h1>
+      </div>
+      
+      <div className="form-lista">
+        <h2>Insira um item:</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <label htmlFor="title">Nome do item</label>
+            <input 
+            type="text" 
+            name="title" 
+            placeholder="Nome do item" 
+            onChange={(e) => setTitle(e.target.value)}
+            value={title || ""}
+            required
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="qtde">Quantidade</label>
+            <input 
+            type="text" 
+            name="title" 
+            placeholder="Quantidade" 
+            onChange={(e) => setTitle(e.target.value)}
+            value={title || ""}
+            required
+            />
+          </div>
+          <input type="submit" value="Enviar" />
+        </form>
+      </div>
+      
+      <div className="">
+        <h2>Lista de itens:</h2>
+        {itens.length === 0 && <p>Não há tarefas</p>}
+      </div>
     </div>
   );
 }
